3b67e92ad3d88686ba93113aace2b8d5f67b7307
MaksAbuzyarov <maksim.abuzyarov@gmail.com>
2019-11-26 12:19:36 +0300

    Add page initialization template in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,19 @@
-
-// -------------------------------------
-//   SVG4Everybody
-// -------------------------------------
 import svg4everybody from 'svg4everybody';
-svg4everybody();
+
+window.Site = {};
+
+// One time after page loading
+window.Site.initPage = function () {
+
+};
+
+// Every time after creating new elements. For example: after ajax loading
+window.Site.initElements = function () {
+  svg4everybody();
+};
+
+window.addEventListener('load', () => {
+  window.Site.initPage();
+  window.Site.initElements();
+});
+
