48b3bd01c8203864eb7571ae66d58e336716f6ae
MaksAbuzyarov <maksim.abuzyarov@gmail.com>
2019-11-29 10:04:43 +0300

    Fix for 'index' task

--- src/views/helpers/index.js
+++ src/views/helpers/index.js
@@ -15,6 +15,6 @@ module.exports.register = function (Handlebars) {
     let relative = path.relative(paths.views.pages, path.relative(file.cwd, path.dirname(file.path)));
     let currentPath = path.join(paths.views.dist, relative);
 
-    return new Handlebars.SafeString('./' + path.relative(currentPath, paths.dist).split('\\').join('/'));
+    return new Handlebars.SafeString(path.relative(currentPath, paths.dist).split('\\').join('/'));
   });
 };
